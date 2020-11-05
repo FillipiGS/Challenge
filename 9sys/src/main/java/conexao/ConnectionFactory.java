426313c38ca0bae3c7104e23cb7468ae43b8b59b
FillipiGS <fillipi.silva@outlook.com>
2020-11-04 21:25:14 -0300

    Adionado classes eclipse

--- 9sys/src/main/java/conexao/ConnectionFactory.java
+++ 9sys/src/main/java/conexao/ConnectionFactory.java
@@ -8,8 +8,8 @@ public class ConnectionFactory {
 
     public Connection DBConnectionManager() throws ClassNotFoundException, SQLException {
         String dbURL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:orcl";
-        String usuario = "RM86110";
-        String senha = "290896";
+        String usuario = "RM85526";
+        String senha = "141099";
         Class.forName("oracle.jdbc.OracleDriver");
         return DriverManager.getConnection(dbURL, usuario, senha);
     }
